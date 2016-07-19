059a1ce2482c69b9b239b1d7fb94816bdf07924e
Mike Rapadas <mike@mrap.me>
2016-07-19 04:18:23 -0700

    Add hero header

--- src/app.js
+++ src/app.js
@@ -1,13 +1,43 @@
-var App = React.createClass({
+'use strict';
+
+const App = React.createClass({
   render: function() {
     return (
-      <div>
-        Hello, world! I am a CommentBox.
+      <div className="page">
+        <div className="container">
+          <div className="row">
+            <div className="one-half column">
+              <HeroTitle
+                title="Mike Rapadas"
+                subtitle="Building Beyond Myself"
+              />
+            </div>
+            <div className="one-half column">
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 });
 
+const HeroTitle = React.createClass({
+  render: function() {
+    return (
+      <div className="container hero-header">
+        <div className="row">
+          <div className="five columns">
+            <img width="122" src="/src/img/avatar.png" alt="Me" className="u-max-full-width"/>
+          </div>
+          <div className="hero-title seven columns">
+            <div className="title">{this.props.title}</div>
+            <div className="subtitle">{this.props.subtitle}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
 ReactDOM.render(
   <App />,
   document.getElementById('content')
